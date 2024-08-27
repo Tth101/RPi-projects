56d3bbad4ca936da51978ed702f9af2dda319cb3
Tth <TTHQSS@gmail.com>
2024-08-28 00:04:18 +0800

    Added memory command and edited frontend

--- CPUStatpage/app/CPUStatpage.py
+++ CPUStatpage/app/CPUStatpage.py
@@ -1,25 +1,15 @@
 import subprocess, re
-#def file_write(content):
-    #f = open("stats.txt", "w")
-   # f.write(content)
-    #f.close()
 
 def tempcheck():
     msg =subprocess.check_output(
         ["../usr/bin/vcgencmd", "measure_temp"]
-    ).decode()#get cpu temperature
+    ).decode() #get cpu temperature
     temp = re.search(r'-?\d\.?\d*', msg) #use regex to obtain temperature
-    out = float(temp.group())
-    return out
+    return temp
 
 def memcheck():
-    msg = "ipconfig"#get cpu temperature
-    mem = re.search(r'-?\d\.?\d*', msg)#use regex to obtain temperature
-    out = float(mem.group())
-    return out
+    msg = subprocess.check_output(
+        ["../usr/bin/free", "-m"]
+    ).decode() # get mem usage
+    return msg
 
-#def init_file():
-    #file_write(tempcheck())
-
-#os.system("ipconfig") #get cpu memory
-#os.system("ipconfig") #get storage
--- CPUStatpage/app/app.py
+++ CPUStatpage/app/app.py
@@ -7,11 +7,12 @@ app = Flask(__name__)
 
 # initialise variables
 temp = CPUStatpage.tempcheck()
+mem = CPUStatpage.memcheck()
 
 # home route that returns below text when root url is accessed
 @app.route("/")
 def hello_world():
-    return render_template('index.html', temp = temp) 
+    return render_template('index.html', temp = temp, mem=mem) 
 
 if __name__ == '__main__':  
    app.run(debug=True)  
