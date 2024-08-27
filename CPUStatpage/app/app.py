90aef83d969ad3c2342fa6174b3d96ec9ceddc1e
Tth <TTHQSS@gmail.com>
2024-08-27 16:39:02 +0800

    testing

--- CPUStatpage/app/app.py
+++ CPUStatpage/app/app.py
@@ -6,12 +6,12 @@ import CPUStatpage
 app = Flask(__name__)
 
 # initialise the text file storing CPU stats
-data = CPUStatpage.init_file()
+#data = CPUStatpage.init_file()
 
 # home route that returns below text when root url is accessed
 @app.route("/")
 def hello_world():
-    return render_template('index.html', data = data)
+    return render_template('index.html')#, data = data
 
 if __name__ == '__main__':  
    app.run(debug=True)  
