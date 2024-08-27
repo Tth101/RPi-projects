f320f3c8b434096da60108e664e7aae3f7c7ba92
Tth <TTHQSS@gmail.com>
2024-08-27 23:26:09 +0800

    testing

--- CPUStatpage/app/CPUStatpage.py
+++ CPUStatpage/app/CPUStatpage.py
@@ -1,23 +1,19 @@
 import subprocess, re
-
-def command_output(command):
-    result = subprocess.run(command, capture_output=True, text=True) #avoid using shell = false?
-    out = result.stdout.strip()
-    return out
-
 #def file_write(content):
     #f = open("stats.txt", "w")
    # f.write(content)
     #f.close()
 
 def tempcheck():
-    msg = command_output("../usr/bin/vcgencmd measure_temp") #get cpu temperature
+    msg =subprocess.check_output(
+        ["../usr/bin/vcgencmd", "measure_temp"]
+    ).decode()#get cpu temperature
     temp = re.search(r'-?\d\.?\d*', msg) #use regex to obtain temperature
     out = float(temp.group())
     return out
 
 def memcheck():
-    msg = command_output("ipconfig")#get cpu temperature
+    msg = "ipconfig"#get cpu temperature
     mem = re.search(r'-?\d\.?\d*', msg)#use regex to obtain temperature
     out = float(mem.group())
     return out
