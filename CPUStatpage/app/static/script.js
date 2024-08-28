dafb061f727a70ef268a953632b9f7044750fecc
Tth <TTHQSS@gmail.com>
2024-08-28 21:42:14 +0800

    troubleshooting

--- CPUStatpage/app/static/script.js
+++ CPUStatpage/app/static/script.js
@@ -1,10 +1,9 @@
 function refresh_variables() {
-    var pythonscript = "../CPUStatpage.py"
-    fetch('/init_stats')
+    fetch('../CPUStatpage.py')
     .then(response => response.json())
         .then(data => {
          // Display the result from the Python function
-         document.getElementById('tmperature').innerText = data.message;
+         document.getElementById('temperature').innerText = data.message;
     })
     .catch(error => {
         console.error('Error:', error);
