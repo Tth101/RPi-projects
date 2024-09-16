f27e722fd5e70fe204f5eb39ca85c7de561f9db6
Tth <TTHQSS@gmail.com>
2024-09-16 16:02:10 +0800

    attempting database operations

--- CPUStatpage/app/app.py
+++ CPUStatpage/app/app.py
@@ -2,35 +2,16 @@ from flask import Flask, render_template, g #g allows for global variables
 import CPUStatpage
 import sqlite3
 
-DATABASE = '/path'
-
-# instance of flask application
-app = Flask(__name__)
-
-# initialise variables
-temp, mem = 0, 1
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+DATABASE = '../app/data_db/cpu-stats-page.db'
+app = Flask(__name__) # instance of flask application
+temp, mem = 0, 1 
 
 def init_stats(temp, mem):
     temp = CPUStatpage.tempcheck()
     mem = CPUStatpage.memcheck()
     return temp, mem
 
-def update_temperature(redis_connection):
-    current_temperature = CPUStatpage.tempcheck()
-    redis_connection.set("currentCPUTemperature", current_temperature)
-
-# home route that returns below text when root url is accessed
-@app.route("/")
-def index():
-    cur = get_db().cursor()
-    return render_template('index.html', temp = init_stats().temp, mem = mem) 
-
+# Database operations
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
@@ -39,6 +20,35 @@ def get_db():
         cursor.execute("SELECT * FROM cpustats")
     return cursor.fetchall()
 
+def main():
+    try:
+        with sqlite3.connect(DATABASE) as conn:
+            data = (CPUStatpage.tempcheck(), CPUStatpage.memcheck())
+            update_db(conn, data)
+    except sqlite3.Error as e:
+        print(e)
+
+
+# home route that returns below text when root url is accessed
+@app.route("/")
+def index():
+    data = str(get_db())
+    return render_template('index.html', temp = init_stats().temp, mem = mem, data = data) 
+
+@app.route("/update")
+def update_db(conn, data):
+    sql = ''' INSERT INTO cpustats (temp, mem, date)
+    VALUES(?, ?, DATE(now))'''
+    cur = conn.cursor()
+    cur.execute(sql, data)
+    conn.commit()
+    return cur.lastrowid
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 if __name__ == '__main__':  
     app.run(debug=True)  
