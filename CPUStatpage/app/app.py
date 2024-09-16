b981d86b3fc484b26516c4fc2ab5fa9ca2631f05
Tth <TTHQSS@gmail.com>
2024-09-16 16:59:17 +0800

    troubleshooting

--- CPUStatpage/app/app.py
+++ CPUStatpage/app/app.py
@@ -1,48 +1,41 @@
-from flask import Flask, render_template, g #g allows for global variables
+from flask import Flask, render_template, redirect, g #g allows for global variables
 import CPUStatpage
 import sqlite3
 
-DATABASE = '../app/db_data'
+DATABASE = '../database/cpu-stats-app.db'
 app = Flask(__name__) # instance of flask application
 temp, mem = 0, 1 
 
-def init_stats(temp, mem):
-    temp = CPUStatpage.tempcheck()
-    mem = CPUStatpage.memcheck()
-    return temp, mem
-
 # Database operations
 def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM cpustats")
-    return cursor.fetchall()
+    conn = sqlite3.connect(DATABASE)
+    c = conn.cursor()
+    c.execute("SELECT * FROM cpustats")
+    rows = c.fetchall()
+    conn.close()
+    print(rows)
+    return rows
 
-def main():
-    try:
-        with sqlite3.connect(DATABASE) as conn:
-            data = (CPUStatpage.tempcheck(), CPUStatpage.memcheck())
-            update_db(conn, data)
-    except sqlite3.Error as e:
-        print(e)
-
+def insert_db(conn, data):
+    sql = '''INSERT INTO cpustats (temp, mem, date)
+    VALUES(?, ?, 'now')'''
+    c = conn.cursor()
+    c.execute(sql, data)
+    conn.commit()
+    conn.close()
 
 # home route that returns below text when root url is accessed
 @app.route("/")
 def index():
     data = str(get_db())
-    return render_template('index.html', temp = init_stats().temp, mem = mem, data = data) 
+    return render_template('index.html', temp = temp, mem = mem, data = data) 
 
 @app.route("/update")
-def update_db(conn, data):
-    sql = ''' INSERT INTO cpustats (temp, mem, date)
-    VALUES(?, ?, DATE(now))'''
-    cur = conn.cursor()
-    cur.execute(sql, data)
-    conn.commit()
-    return cur.lastrowid
+def generate_stats():
+    conn = sqlite3.connect(DATABASE)
+    data = (CPUStatpage.tempcheck(), CPUStatpage.memcheck())
+    insert_db(conn, data)
+    return data
 
 @app.teardown_appcontext
 def close_connection(exception):
--- CPUStatpage/readme.md
+++ CPUStatpage/readme.md
@@ -47,6 +47,7 @@ portainer community edition 2.19.4
 
 ## Important tip(s)/Useful information/References
 1. Do not name your `WORKDIR` in `dockerfile` the same as your folders in your project repo: troubleshooting will be quit difficult
+2. Specifying volume in docker compose is `src path`:`dst path`
 2. On python's `subprocess`: 
     * [Shell = true or Shell = false?](https://stackoverflow.com/questions/3172470/actual-meaning-of-shell-true-in-subprocess)
     * [Running commands](https://martinheinz.dev/blog/98)
