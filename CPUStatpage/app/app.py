9198d8daccaaf3698cc69bc89885b125db90aa82
Tth <TTHQSS@gmail.com>
2024-09-19 13:40:09 +0800

    styling front end & fixing script

--- CPUStatpage/app/app.py
+++ CPUStatpage/app/app.py
@@ -18,7 +18,7 @@ def get_db():
 
 def insert_db(conn, data):
     sql = '''INSERT INTO cpustats (temp, mem, date)
-    VALUES(?, ?, DATE('now'))'''
+    VALUES(?, ?, DATETIME('now'))'''
     c = conn.cursor()
     c.execute(sql, data)
     conn.commit()
