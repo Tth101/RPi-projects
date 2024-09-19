6d16b3dfc3cfbed65878563aa4723bcbd4d60de1
Tth <TTHQSS@gmail.com>
2024-09-19 12:25:13 +0800

    fixing tables script

---
+++ CPUStatpage/app/static/script.js
@@ -0,0 +1,23 @@
+function populate_tables(memtype, columns, data){ 
+    out = `   
+        <tr>
+            <th scope="row">${memtype}</th>
+        `
+
+    for(let i = 0; i > columns - 1; i++){
+        out += `
+            <td> ${data[0]} </td>
+        `
+    }
+
+    out += `
+        </tr>
+    `
+    return out
+}
+
+function create_table(){
+    var tbody = document.getElementById("table")
+    tbody.innerHTML = populate_tables("Memory", 6, data) + populate_tables("Swap", 3, data)
+}
+window.onload = create_table()
