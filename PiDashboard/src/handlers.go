5d5a67d5ca311da61344924141356352bf96c99e
Tth <TTHQSS@gmail.com>
2026-03-22 10:19:29 +0800

    added cors headers

--- PiDashboard/src/handlers.go
+++ PiDashboard/src/handlers.go
@@ -6,18 +6,21 @@ import (
 )
 
 func getNewHC(w http.ResponseWriter, r *http.Request) {
+    //Needs CORS for cross domain requests
     logger.Info().Msg("getNewHC handler called");
     hcResult := healthcheck();
     jsonResult := toJson(hcResult);
     postResults(jsonResult);
 
     w.Header().Set("Content-Type", "application/json");
+    w.Header().Set("Access-Control-Allow-Origin", "*");
     w.Write(jsonResult);  
     
     logger.Info().Msgf("getNewHC handler: %v", hcResult);
 }
 
 func getInfoContent(w http.ResponseWriter, r *http.Request) {
+    //Needs CORS for cross domain requests
     logger.Info().Msg("getInfoContent handler called");
     infoResult := getContent();
     jsonResult := toJson(infoResult);
